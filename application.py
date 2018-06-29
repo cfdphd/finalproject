93bacd545cf2b7095f97419d4f205dd2358f27d8
cfdphd <cfdphd@outlook.com>
2018-06-29 17:23:32 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,124 @@
+import os
+import re
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
+from flask_jsglue import JSGlue
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+JSGlue(app)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
+
+
+@app.route("/")
+def index():
+    """Render map."""
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API_KEY not set")
+    return render_template("index.html", key=os.environ.get("API_KEY"))
+
+
+
+@app.route("/happy", methods=["GET", "POST"])
+def happy():
+
+    if request.method == "GET":
+        return render_template("happy.html")
+    else:
+        return redirect(url_for("index"))
+
+
+@app.route("/neither", methods=["GET", "POST"])
+def neither():
+    """Neither happy nor depressed"""
+    if request.method == "GET":
+        return render_template("neither.html")
+    else:
+        return render_template("quoted.html")
+
+
+
+@app.route("/depressed", methods=["GET", "POST"])
+def depressed():
+    """Recommend places for a depressed person"""
+    if request.method == "GET":
+        return render_template("depressed.html")
+    else:
+        name = request.form.get("name")
+        """address them and tell them you will do something"""
+
+        return relief1(name, "you will feel better")
+
+        return render_template("demobox.html")
+
+
+@app.route("/update")
+def update():
+    """Find up to 10 places within view."""
+
+    # ensure parameters are present
+    if not request.args.get("sw"):
+        raise RuntimeError("missing sw")
+    if not request.args.get("ne"):
+        raise RuntimeError("missing ne")
+
+    # ensure parameters are in lat,lng format
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("sw")):
+        raise RuntimeError("invalid sw")
+    if not re.search("^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$", request.args.get("ne")):
+        raise RuntimeError("invalid ne")
+
+    # explode southwest corner into two variables
+    (sw_lat, sw_lng) = [float(s) for s in request.args.get("sw").split(",")]
+
+    # explode northeast corner into two variables
+    (ne_lat, ne_lng) = [float(s) for s in request.args.get("ne").split(",")]
+
+    # find 10 cities within view, pseudorandomly chosen if more within view
+    if (sw_lng <= ne_lng):
+
+        # doesn't cross the antimeridian
+        rows = db.execute("""SELECT * FROM places
+            WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude AND longitude <= :ne_lng)
+            GROUP BY country_code, place_name, admin_code1
+            ORDER BY RANDOM()
+            LIMIT 10""",
+            sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    else:
+
+        # crosses the antimeridian
+        rows = db.execute("""SELECT * FROM places
+            WHERE :sw_lat <= latitude AND latitude <= :ne_lat AND (:sw_lng <= longitude OR longitude <= :ne_lng)
+            GROUP BY country_code, place_name, admin_code1
+            ORDER BY RANDOM()
+            LIMIT 10""",
+            sw_lat=sw_lat, ne_lat=ne_lat, sw_lng=sw_lng, ne_lng=ne_lng)
+
+    # output places as JSON
+    return jsonify(rows)
